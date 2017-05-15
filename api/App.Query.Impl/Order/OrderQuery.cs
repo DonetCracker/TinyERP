d09d1dba2ff934bad031bd1ed2fcf668ae157056
TU Tran <contact@tranthanhtu.vn>
2017-05-15 15:48:31 +0700

    Update event handler for order

--- api/App.Query.Impl/Order/OrderQuery.cs
+++ api/App.Query.Impl/Order/OrderQuery.cs
@@ -1,54 +1,30 @@
 ﻿namespace App.Query.Impl.Order
 {
-    using System;
     using App.Common.Data;
     using App.Common.Data.MongoDB;
     using App.Query.Order;
     using App.Query.Entity.Order;
-    using System.Linq;
-    using ValueObject.Order;
     using System.Collections.Generic;
     using Common.Mapping;
+    using System;
+    using System.Linq;
 
     public class OrderQuery : BaseQueryRepository<Order>, IOrderQuery
     {
         public OrderQuery() : base(new MongoDbContext()) { }
 
-        public void ActivateOrder(Guid orderId)
+        public Order GetByOrderId(Guid orderId)
         {
-            App.Query.Entity.Order.Order order = this.DbSet.AsQueryable().FirstOrDefault(item => item.OrderId == orderId);
-            order.IsActivated = true;
-            this.DbSet.Update(order);
-        }
-
-        public void AddOrderLineItem(Guid orderId,Guid productId,string productName,int quantity, decimal price)
-        {
-            App.Query.Entity.Order.Order order = this.DbSet.AsQueryable().FirstOrDefault(item => item.OrderId == orderId);
-            order.OrderLines.Add(new OrderLine(productId,productName, quantity, price));
-            order.TotalItems += quantity;
-            order.TotalPrice += price*(decimal)quantity;
-            this.DbSet.Update(order);
-        }
-        public void CreateOrder(Guid orderId)
-        {
-            this.DbSet.Add(new App.Query.Entity.Order.Order(orderId));
+            return this.DbSet.AsQueryable().FirstOrDefault(item => item.OrderId == orderId);
         }
 
         public TEntity GetOrder<TEntity>(string id) where TEntity : IMappedFrom<Order>
         {
             return this.GetById<TEntity>(id);
         }
-
         public IList<TEntity> GetOrders<TEntity>() where TEntity : IMappedFrom<Order>
         {
             return this.GetItems<TEntity>();
-        }
-
-        public void UpdateCustomerDetail(Guid orderId, string customerName)
-        {
-            App.Query.Entity.Order.Order order = this.DbSet.AsQueryable().FirstOrDefault(item => item.OrderId == orderId);
-            order.Name = customerName;
-            this.DbSet.Update(order);
         }
     }
 }
--- api/App.Query/Order/IOrderQuery.cs
+++ api/App.Query/Order/IOrderQuery.cs
@@ -1,16 +1,15 @@
 ﻿namespace App.Query.Order
 {
+    using Common.Data;
     using Common.Mapping;
-    using System;
     using System.Collections.Generic;
+    using Entity.Order;
+    using System;
 
-    public interface IOrderQuery
+    public interface IOrderQuery: IBaseQueryRepository<App.Query.Entity.Order.Order>
     {
-        void CreateOrder(Guid orderId);
-        void AddOrderLineItem(Guid orderId,Guid productId,string productName,int quantity, decimal price);
-        void UpdateCustomerDetail(Guid orderId, string customerName);
-        void ActivateOrder(Guid orderId);
         IList<TEntity> GetOrders<TEntity>() where TEntity : IMappedFrom<App.Query.Entity.Order.Order>;
         TEntity GetOrder<TEntity>(string id) where TEntity : IMappedFrom<App.Query.Entity.Order.Order>;
+        Order GetByOrderId(Guid orderId);
     }
 }
