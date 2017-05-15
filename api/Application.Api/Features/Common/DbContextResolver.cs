93fb9c31fc87925567f2f939886ac088c1d8089c
TU Tran <contact@tranthanhtu.vn>
2017-05-15 16:33:12 +0700

    add DbContextFactory

---
+++ api/Application.Api/Features/Common/DbContextResolver.cs
@@ -0,0 +1,22 @@
+﻿namespace App.Api.Features.Common
+{
+    using System;
+    using App.Common;
+    using App.Common.Data;
+    using Context;
+
+    public class DbContextResolver : IDbContextResolver
+    {
+        public IDbContext Resolve(RepositoryType type)
+        {
+            switch (type) {
+                case RepositoryType.MSSQL:
+                    return new AppDbContext();
+                case RepositoryType.MongoDb:
+                    return new App.Common.Data.MongoDB.MongoDbContext();
+                default:
+                    throw new InvalidOperationException("common.errors.unsupportedTyeOdDbContext");
+            }
+        }
+    }
+}
