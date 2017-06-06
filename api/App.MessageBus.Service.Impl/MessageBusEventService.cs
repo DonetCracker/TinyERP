3cb2ffbffbd3b45c4e8374397950a057db9d0d94
TU Tran <contact@tranthanhtu.vn>
2017-06-06 14:16:56 +0700

    Add api for mesage bus

---
+++ api/App.MessageBus.Service.Impl/MessageBusEventService.cs
@@ -0,0 +1,14 @@
+﻿namespace App.MessageBus.Service.Impl
+{
+    using System;
+    using Common.MessageBus;
+    using App.MessageBus.Service;
+    internal class MessageBusEventService : IMessageBusEventService
+    {
+        public CreateMessageBusEventResponse Create(MessageBusEvent ev)
+        {
+            this.ValidateCreateMessageBusEventRequest(ev);
+            using (IUnitOfWork uow = Unit)
+        }
+    }
+}
---
+++ api/App.RESTMessageBus/Controllers/MessageBusEventsController.cs
@@ -0,0 +1,22 @@
+﻿namespace App.RESTMessageBus.Controllers
+{
+    using App.Common.MVC;
+    using MessageBus.Service;
+    using Common.DI;
+    using Common.MessageBus;
+    using Common.MVC.Attributes;
+    using System.Web.Http;
+
+    [RoutePrefix("api/messageBus")]
+    public class MessageBusEventsController : BaseApiController
+    {
+        [HttpPost]
+        [Route("")]
+        [ResponseWrapper()]
+        public CreateMessageBusEventResponse CreateMessage(MessageBusEvent ev)
+        {
+            IMessageBusEventService service = IoC.Container.Resolve<IMessageBusEventService>();
+            return service.Create(ev);
+        }
+    }
+}
